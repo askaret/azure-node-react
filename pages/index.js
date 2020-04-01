b8a1feefff349e2c6ea1d8995ba3a05778481a8c
Andreas Skaret <andreas.skaret@gmail.com>
2020-04-01 14:00:37 +0200

    Added more from tutorial

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,22 @@
 import Layout from '../components/layout';
+import Link from 'next/link';
 
-export default function Index() {
+const PostLink = props => (
+    <li>
+        <Link href={`/post?title=${props.title}&poop=${props.poop}`}>
+            <a>{props.title}</a>
+        </Link>
+    </li>
+);
+export default function Blog() {
     return (
         <Layout>
-            <p>Hello Next.js</p>
-
+            <h1>My Blog</h1>
+            <ul>
+                <PostLink title="Hello Next.js" poop="fuckaroni"/>
+                <PostLink title="Learn Next.js is awesome" />
+                <PostLink title="Deploy apps with Zeit" />
+            </ul>
         </Layout>
     );
 }
---
+++ pages/post.js
@@ -0,0 +1,34 @@
+import { useRouter } from 'next/router';
+import Layout from '../components/layout';
+
+const Content = () => {
+    const router = useRouter();
+    return (
+        <>
+            <h1>{router.query.title}</h1>
+            <p>This is the blåg post content.</p>
+            <b>{router.query.poop}</b>
+        </>
+    );
+};
+
+const Page = () => (
+    <Layout>
+        <Content />
+    </Layout>
+);
+
+/*
+const Page = () => {
+    const router = useRouter();
+
+    return (
+        <Layout>
+            <h1>{router.query.title}</h1>
+            <p>This is the blog post content.</p>
+            <b>{router.query.poop}</b>
+        </Layout>
+    );
+};
+*/
+export default Page;
